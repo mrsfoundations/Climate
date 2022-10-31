3e3eacabbd2f86e8bf22701e9448ad9c527d7b91
Vimalkumar12610 <117089088+Vimalkumar12610@users.noreply.github.com>
2022-10-31 18:28:49 +0530

    hf22256

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: LoadingScreen(),
     );
   }
